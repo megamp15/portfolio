164ef09bd2bfd032e12653603c21f4c0302d14e8
Mahir Pirmohammed <pmahir15@gmail.com>
2024-05-02 12:06:38 -0500

    Updating basepath for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 // /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", 
-    // basePath: "/nextjs14-github-pages-starter", // Comment this during local development. When uploading to Github make sure this is the repo name /<repo name>
+    basePath: "/portfolio", // Comment this during local development. When uploading to Github make sure this is the repo name /<repo name>
     images: {
         unoptimized: true,
     }
